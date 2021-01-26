32208dcd5224f382a77e7766ef0ed0ae5718ab48
mlobo.dev <mlobo.dev@gmail.com>
2021-01-26 07:52:13 -0300

    Springboot init project

---
+++ src/main/java/com/dev4abyss/csvreader/CsvreaderApplication.java
@@ -0,0 +1,13 @@
+package com.dev4abyss.csvreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvreaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsvreaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev4abyss/csvreader/CsvreaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev4abyss.csvreader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvreaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
